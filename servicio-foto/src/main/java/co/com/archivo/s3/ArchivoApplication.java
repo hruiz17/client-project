4a0b1520359c04f78b2e0940f94a1400054af7b5
humberto.ruiz <hruiz17@hotmail.com.co>
2021-07-21 13:51:35 -0500

    version inicial proyecto foto

---
+++ servicio-foto/src/main/java/co/com/archivo/s3/ArchivoApplication.java
@@ -0,0 +1,13 @@
+package co.com.archivo.s3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArchivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArchivoApplication.class, args);
+	}
+
+}
---
+++ servicio-foto/src/test/java/co/com/archivo/s3/ArchivoApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.archivo.s3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArchivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
