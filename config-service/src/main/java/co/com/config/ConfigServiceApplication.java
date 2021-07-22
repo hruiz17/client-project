3d50c10add813421789d9b7d6548db132a8e4435
humberto.ruiz <hruiz17@hotmail.com.co>
2021-07-21 21:08:18 -0500

    ajustes al config-service

--- config-service/src/main/java/co/com/config/ConfigServiceApplication.java
+++ config-service/src/main/java/co/com/config/ConfigServiceApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.config.server.EnableConfigServer;
 
 @EnableConfigServer
 @SpringBootApplication
-public class ServiceConfigApplication {
+public class ConfigServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceConfigApplication.class, args);
+		SpringApplication.run(ConfigServiceApplication.class, args);
 	}
 
 }
