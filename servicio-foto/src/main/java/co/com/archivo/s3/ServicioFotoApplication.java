060778c5d7242aded2093c72ec3f4f27e36d8c70
humberto.ruiz <hruiz17@hotmail.com.co>
2021-07-21 21:12:52 -0500

    se ajusta el servicio foto

--- servicio-foto/src/main/java/co/com/archivo/s3/ServicioFotoApplication.java
+++ servicio-foto/src/main/java/co/com/archivo/s3/ServicioFotoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ArchivoApplication {
+public class ServicioFotoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ArchivoApplication.class, args);
+		SpringApplication.run(ServicioFotoApplication.class, args);
 	}
 
 }
