1e3e418b2bab0f70d1e0ae76383e0b0e85b5ba9d
humberto.ruiz <hruiz17@hotmail.com.co>
2021-07-22 11:07:19 -0500

    ajuste en la configuracion

--- servicio-cliente/src/main/java/co/com/cliente/ServicioClienteApplication.java
+++ servicio-cliente/src/main/java/co/com/cliente/ServicioClienteApplication.java
@@ -1,8 +1,19 @@
 package co.com.cliente;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableHystrix
+@EnableHystrixDashboard
+@EnableEurekaClient
+@EnableFeignClients
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class ServicioClienteApplication {
 
